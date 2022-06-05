e1ed9ee2a44b24e8e480252d819addedc889b774
João Pedro A <jaaopbr@gmail.com>
2022-06-05 13:44:37 -0300

    🎉 Initial project structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./app/src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        black: '#1e2022',
+        purple: '#302e53',
+        orange: '#d07017'
+      },
+      fontFamily: {
+        sans: ['Roboto Condensed', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
