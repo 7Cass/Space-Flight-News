667c7e0b71d9aa2ba48bfef49effc13669fa420c
João Pedro A <jaaopbr@gmail.com>
2022-06-05 21:21:43 -0300

    💄 Updated layout styles

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import SearchBox from './SearchBox';
 export function Header() {
 
     return (
-        <header className="w-full flex items-center justify-end mx-auto shadow-lg h-20 px-6 mt-4 rounded-2xl">
+        <header className="w-full flex items-center justify-end shadow h-24 px-6">
             <SearchBox />
             <DropDownSort />
         </header >
